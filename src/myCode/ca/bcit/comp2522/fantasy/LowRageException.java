70d16ec11401f6adcde0d6244cf854528543afec
arshiaadamian <arshiaadamian@gmail.com>
2025-09-21 13:50:21 -0700

    finished the Orc class

---
+++ src/myCode/ca/bcit/comp2522/fantasy/LowRageException.java
@@ -0,0 +1,9 @@
+package ca.bcit.comp2522.fantasy;
+
+public class LowRageException extends RuntimeException
+{
+    public LowRageException(String message)
+    {
+        super(message);
+    }
+}
--- src/myCode/ca/bcit/comp2522/fantasy/Orc.java
+++ src/myCode/ca/bcit/comp2522/fantasy/Orc.java
@@ -2,6 +2,7 @@ package ca.bcit.comp2522.fantasy;
 
 public class Orc extends Creature
 {
+    private int rage;
     public Orc (final String name, final Date dateOfBirth, final int health, int rage)
     {
         super (name, dateOfBirth, health);
@@ -9,7 +10,27 @@ public class Orc extends Creature
         {
             throw new IllegalArgumentException("Rage cannot be negative");
         }
-
-        @Override;
+        this.rage = rage;
     }
+
+    @Override
+    public void getDetails()
+    {
+        super.getDetails();
+        System.out.println("Rage: " + rage);
+    }
+
+    public void bersek() {
+        rage += 5;
+        if (rage > 20)
+        {
+            super.takeDamage(30);
+        }
+        else if (rage < 5)
+        {
+            throw new LowRageException("Rage cannot be below 5");
+        }
+    }
+
+
 }
